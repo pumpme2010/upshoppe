14c14e78fe1f29a74ca8faabf62e8ce9567e6176
Jon Patrick Lim <cirpat007@gmail.com>
2013-04-30 16:07:43 +0800

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Proj::Application.routes.draw do
+
+  root :to => 'sessions#new'
+  
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "pages/about"
   get "pages/newsfeed"
@@ -78,7 +81,6 @@ Proj::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'sessions#new'
 
   # See how all your routes lay out with "rake routes"
 
