21a86716a49f28c78171f5a2bda4ce91c4248c39
Jon Patrick Lim <cirpat007@gmail.com>
2013-05-01 11:02:08 +0800

    changed migration

--- db/migrate/20130306075119_add_image_to_users.rb
+++ db/migrate/20130306075119_add_image_to_users.rb
@@ -1,5 +1,10 @@
 class AddImageToUsers < ActiveRecord::Migration
   def change
     add_column :users, :image, :string
+    
+  end
+
+  def migrate
+  	User.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :admin=> true)
   end
 end
